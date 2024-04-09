af79e92659df4e3004277fbb81a83edbe8e6b078
Avinash Wagh <avinash.wagh@ayssoftwaresolution.com>
2024-04-09 08:27:36 +0530

    first action

---
+++ webscraper.io cloud api/config.js
@@ -0,0 +1,6 @@
+import { Client } from "@webscraperio/api-client-nodejs";
+
+export const client = new Client({
+	token: "o31qwOFJmShGsXvjwroK8MWb2CHbZoSoc8Esv2kKiF2n6f4uss62zrowPogf",
+	useBackoffSleep: false
+});
--- webscraper.io cloud api/rest-scripts/scape-job.script.js
+++ webscraper.io cloud api/rest-scripts/scape-job.script.js
@@ -1,14 +1,8 @@
-const axios = require('axios');
+const { client } = require('../config');
 
 async function startScrapingJob() {
     try {
-        const API_TOKEN = process.env.WEBSCRAPER_API_TOKEN; // Assuming you have your API token stored in an environment variable
-
-        const response = await axios.post('https://api.webscraper.io/api/v1/scraping-job', {
-            api_token: API_TOKEN,
-            // Add other parameters as required by the Webscraper.io API
-        });
-
+        const response = await client.getSitemaps();
         console.log('Scraping job started successfully:', response.data);
     } catch (error) {
         console.error('Error starting scraping job:', error.message);
