b5bdc705b6fcea138cd59a37657143ac9bac2042
Avinash Wagh <avinash.wagh@ayssoftwaresolution.com>
2024-04-09 08:33:23 +0530

    import change

--- webscraper.io cloud api/config.js
+++ webscraper.io cloud api/config.js
@@ -1,4 +1,5 @@
-import { Client } from "@webscraperio/api-client-nodejs";
+const api = require("@webscraperio/api-client-nodejs");
+const Client = api.Client;
 
 export const client = new Client({
 	token: "o31qwOFJmShGsXvjwroK8MWb2CHbZoSoc8Esv2kKiF2n6f4uss62zrowPogf",
