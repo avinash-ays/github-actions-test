7d235e4d310b59a105c15be098615a9e02bf9e52
Avinash Wagh <avinash.wagh@ayssoftwaresolution.com>
2024-04-09 08:35:16 +0530

    test

--- webscraper.io cloud api/config.js
+++ webscraper.io cloud api/config.js
@@ -1,7 +1,9 @@
 const api = require("@webscraperio/api-client-nodejs");
 const Client = api.Client;
 
-export const client = new Client({
-	token: "o31qwOFJmShGsXvjwroK8MWb2CHbZoSoc8Esv2kKiF2n6f4uss62zrowPogf",
-	useBackoffSleep: false
+const client = new Client({
+    token: "o31qwOFJmShGsXvjwroK8MWb2CHbZoSoc8Esv2kKiF2n6f4uss62zrowPogf",
+    useBackoffSleep: false
 });
+
+module.exports = client;
