a4561f29edae17646c6c11267900d017cb634d82
Avinash Wagh <avinash.wagh@ayssoftwaresolution.com>
2024-04-10 15:29:45 +0530

    test

--- webscraper.io cloud api/rest-scripts/scrape-job.script.js
+++ webscraper.io cloud api/rest-scripts/scrape-job.script.js
@@ -1,53 +1,26 @@
+const fs = require('fs')
 const client = require("../config");
+const Sitemaps = []
+async function getSitemaps() {
+  //get the sitemaps from cloud.webscraper.io
+  let generator = client.getSitemaps();
+  Sitemaps = await generator.getAllRecords();
 
-async function updateSitemaps() {
-    //get the sitemaps from cloud.webscraper.io
-    let generator = client.getSitemaps();
-    const sitemaps = await generator.getAllRecords();
-
-    console.log("SITEMAPS", sitemaps);
+  console.log("SITEMAPS", Sitemaps);
 }
-
-updateSitemaps();
-
-const fs = require('fs');
-const path = require('path');
-
-async function getUpdatedFiles() {
-  // Assuming the script file is located two levels above the scrapper folder
-  const scraperPath = path.join(__dirname, '../../scrapper');
-  const updatedFiles = [];
-
-  try {
-    // Get the list of files in the scraper directory
-    console.log("scraper path : ",scraperPath);
-    const files = fs.readdirSync(scraperPath);
-
-    // Process each file
-    files.forEach((file) => {
-      const filePath = path.join(scraperPath, file);
-
-      // Check if the file was modified after the last commit/push
-      const modifiedTime = fs.statSync(filePath).mtime;
-      const lastPushTime = getLastPushTime(); // Implement a function to get the last push time
-
-      if (modifiedTime > lastPushTime) {
-        updatedFiles.push(file); // Collect updated file names
-      }
-    });
-  } catch (error) {
-    console.error('Error reading directory:', error);
+getSitemaps();
+//reading files from scrapper folder to check diff
+function getFiles(dir, files = []) {
+  const fileList = fs.readdirSync(dir)
+  for (const file of fileList) {
+    file
+    if (fs.statSync(file).isDirectory()) {
+      getFiles(file, files)
+    } else {
+      files.push(file)
+    }
   }
-
-  return updatedFiles;
+  return files
 }
-
-function getLastPushTime() {
-  // Implement logic to get the time of the last commit/push
-  // You can use git commands or retrieve it from your version control system
-}
-
-(async () => {
-  const updatedFiles = await getUpdatedFiles();
-  console.log('Updated files:', updatedFiles);
-})();
+const filesInFolder = getFiles('scrapper')
+console.log("scrapper/files :----- ",filesInFolder);
