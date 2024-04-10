d4aec8e13f70ff8e36ba2416abafbe3ad245bae3
Avinash Wagh <avinash.wagh@ayssoftwaresolution.com>
2024-04-11 00:03:04 +0530

    test

--- webscraper.io cloud api/rest-scripts/util.js
+++ webscraper.io cloud api/rest-scripts/util.js
@@ -1,4 +1,5 @@
 const fs = require('fs').promises;
+const { execSync } = require('child_process');
 
 async function readLocalSitemaps(dir) {
     try {
@@ -13,6 +14,26 @@ async function readLocalSitemaps(dir) {
         throw error;
     }
 }
+
+function identifyModifiedFiles() {
+    try {
+        // Use git diff to get the list of modified files
+        const modifiedFiles = execSync('git diff --name-only HEAD^ HEAD').toString().trim().split('\n');
+        
+        // Return the modified files
+        return modifiedFiles;
+    } catch (error) {
+        console.error('Error identifying modified files:', error);
+        return [];
+    }
+}
+
+// Call the function to identify modified files
+const modifiedFiles = identifyModifiedFiles();
+console.log('Modified Files:');
+console.log(modifiedFiles.join('\n'));
+
+
 
 module.exports = {
     readLocalSitemaps
