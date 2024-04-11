37bdcb7446f439f828c0d76648b63fd30cbed942
Avinash Wagh <avinash.wagh@ayssoftwaresolution.com>
2024-04-11 10:19:03 +0530

    test

--- webscraper.io cloud api/rest-scripts/scrape-job.script.js
+++ webscraper.io cloud api/rest-scripts/scrape-job.script.js
@@ -16,29 +16,29 @@ async function findDiffSitemaps(local, cloud) {
 
 async function main() {
   try {
-    const cloudSitemaps = await getSitemaps();
-    const localSitemaps = await readLocalSitemaps('scrapper');
+    // const cloudSitemaps = await getSitemaps();
+    // const localSitemaps = await readLocalSitemaps('scrapper');
 
-    // Stringify data of local sitemaps
-    const stringifiedLocalSitemaps = localSitemaps.map(sitemap => ({
-      name: sitemap.name,
-      data: JSON.stringify(sitemap.data)
-    }));
-    // 
-    const { localValuesNotInCloud, cloudValuesNotInLocal } = await findDiffSitemaps(stringifiedLocalSitemaps, cloudSitemaps);
+    // // Stringify data of local sitemaps
+    // const stringifiedLocalSitemaps = localSitemaps.map(sitemap => ({
+    //   name: sitemap.name,
+    //   data: JSON.stringify(sitemap.data)
+    // }));
+    // // 
+    // const { localValuesNotInCloud, cloudValuesNotInLocal } = await findDiffSitemaps(stringifiedLocalSitemaps, cloudSitemaps);
 
-    //delete sitemap which are not present in local scrapper folder
-    for (const cloud of cloudValuesNotInLocal) {
-      await deleteSitemap(cloud.id);
-    }
+    // //delete sitemap which are not present in local scrapper folder
+    // for (const cloud of cloudValuesNotInLocal) {
+    //   await deleteSitemap(cloud.id);
+    // }
 
-    // Create sitemaps which are not in cloud
-    for (const local of localValuesNotInCloud) {
-      //create a new sitemap
-      const sitemap = await createSitemap(local.data);
-      //start new scrap-job for new sitemap created
-      await createScrapJob(sitemap.id, JSON.parse(local.data))
-    }
+    // // Create sitemaps which are not in cloud
+    // for (const local of localValuesNotInCloud) {
+    //   //create a new sitemap
+    //   const sitemap = await createSitemap(local.data);
+    //   //start new scrap-job for new sitemap created
+    //   await createScrapJob(sitemap.id, JSON.parse(local.data))
+    // }
 
     // Call the function to identify modified files in the "myFolder" folder
     console.log('calling diff function');
