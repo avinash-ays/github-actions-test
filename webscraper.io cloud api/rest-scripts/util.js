94386bfe8918187e103377fe85ee209e4db4c914
Avinash Wagh <avinash.wagh@ayssoftwaresolution.com>
2024-04-11 12:06:35 +0530

    test

--- webscraper.io cloud api/rest-scripts/util.js
+++ webscraper.io cloud api/rest-scripts/util.js
@@ -2,40 +2,37 @@ const fs = require('fs').promises;
 const { execSync } = require('child_process');
 
 async function readLocalSitemaps(dir) {
-  try {
-    const files = await fs.readdir(dir);
-    const fileObjects = await Promise.all(files.map(async file => {
-      const data = await fs.readFile(`${dir}/${file}`, 'utf8');
-      return { name: file.replace(/\.json$/, ""), data: JSON.parse(data) };
-    }));
-    return fileObjects;
-  } catch (error) {
-    console.error("Error reading local sitemap files:", error);
-    throw error;
-  }
+    try {
+        const files = await fs.readdir(dir);
+        const fileObjects = await Promise.all(files.map(async file => {
+            const data = await fs.readFile(`${dir}/${file}`, 'utf8');
+            return { name: file.replace(/\.json$/, ""), data: JSON.parse(data) };
+        }));
+        return fileObjects;
+    } catch (error) {
+        console.error("Error reading local sitemap files:", error);
+        throw error;
+    }
 }
 
 // Function to identify updated files within a specific folder
 function identifyUpdatedFiles() {
-    try {
-        // Run the Git command to identify updated files within the scrapper folder
-        const command = `git diff --name-only HEAD^`;
-        const updatedFiles = execSync(command, { encoding: 'utf-8' });
+  try {
+    // Run the Git command to identify updated files
+ 
+    const command = `git diff --name-only HEAD origin/main -- scrapper/`;
+    const updatedFiles = execSync(command, { encoding: 'utf-8' });
 
-        // Split the output by newline character to get a list of updated file names
-        const updatedFileList = updatedFiles.trim().split('\n');
-        
-        // Print the list of updated files
-        console.log(`Updated files within the scrapper folder:`);
-        console.log(updatedFileList);
-
-        return updatedFileList;
-    } catch (error) {
-        console.error('Error identifying updated files:', error);
-    }
+    const updatedFileList = updatedFiles.trim().split('\n');
+    console.log(`Updated files within the folder scrapper:`);
+    console.log(updatedFileList);
+    return updatedFileList;
+  } catch (error) {
+    console.error('Error identifying updated files:', error);
+  }
 }
 
 module.exports = {
-  readLocalSitemaps,
-  identifyUpdatedFiles
+    readLocalSitemaps,
+    identifyUpdatedFiles
 };
