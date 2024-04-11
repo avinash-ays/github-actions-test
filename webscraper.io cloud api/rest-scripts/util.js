b91fe6264b78614a5642c82c7ec3da801ed2f3bf
Avinash Wagh <avinash.wagh@ayssoftwaresolution.com>
2024-04-11 10:40:09 +0530

    test

--- webscraper.io cloud api/rest-scripts/util.js
+++ webscraper.io cloud api/rest-scripts/util.js
@@ -16,30 +16,29 @@ async function readLocalSitemaps(dir) {
 }
 
 // Function to identify updated files within a specific folder
-
 function identifyUpdatedFiles() {
-    try {
-        // Get the commit hash for HEAD dynamically
-        const headHash = execSync('git rev-parse HEAD', { encoding: 'utf-8' }).trim();
-        
-        // Get the commit hash for the parent commit dynamically
-        const parentHash = execSync('git log --pretty=%H -n 1 HEAD^', { encoding: 'utf-8' }).trim();
+  try {
+      // Get the commit hash for HEAD dynamically
+      const headHash = execSync('git rev-parse HEAD', { encoding: 'utf-8' }).trim();
+      
+      // Get the commit hash for the parent commit dynamically
+      const parentHash = execSync('git rev-parse HEAD~1', { encoding: 'utf-8' }).trim();
 
-        // Run the Git command to identify updated files within the scrapper folder
-        const command = `git diff --name-only ${parentHash} ${headHash} -- scrapper/`;
-        const updatedFiles = execSync(command, { encoding: 'utf-8' });
+      // Run the Git command to identify updated files within the scrapper folder
+      const command = `git diff --name-only ${parentHash} ${headHash} -- scrapper/`;
+      const updatedFiles = execSync(command, { encoding: 'utf-8' });
 
-        // Split the output by newline character to get a list of updated file names
-        const updatedFileList = updatedFiles.trim().split('\n');
-        
-        // Print the list of updated files
-        console.log(`Updated files within the scrapper folder:`);
-        console.log(updatedFileList);
+      // Split the output by newline character to get a list of updated file names
+      const updatedFileList = updatedFiles.trim().split('\n');
+      
+      // Print the list of updated files
+      console.log(`Updated files within the scrapper folder:`);
+      console.log(updatedFileList);
 
-        return updatedFileList;
-    } catch (error) {
-        console.error('Error identifying updated files:', error);
-    }
+      return updatedFileList;
+  } catch (error) {
+      console.error('Error identifying updated files:', error);
+  }
 }
 
 module.exports = {
